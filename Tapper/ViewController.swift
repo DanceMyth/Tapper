4badff4d0bba4b0898e9bd0366e126db7de2b08e
Dan Smith <Dancemyth@icloud.com>
2015-12-06 01:43:32 +0000

    working

--- Tapper/ViewController.swift
+++ Tapper/ViewController.swift
@@ -9,17 +9,83 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //properties
+    var maxTaps: Int = 0
+    var currentTaps: Int = 0
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var logoimg: UIImageView!
+    @IBOutlet weak var howManyTapsTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var tapsBtn: UIButton!
+    @IBOutlet weak var tapsLbl: UILabel!
+    
+    
+    
+    @IBAction func coinTapped(sender: UIButton!){
+        currentTaps += 1
+        updateTapsLbl()
+        if isGameOver(){
+          restartGame()
+        }
+        
+    }
+    
+    @IBAction func onPlayBtnPressed(sender: UIButton!){
+
+        
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != ""{
+            logoimg.hidden = true
+            playBtn.hidden = true
+            howManyTapsTxt.hidden = true
+            
+            tapsBtn.hidden = false
+            tapsLbl.hidden = false
+            
+            
+            maxTaps = Int(howManyTapsTxt.text!)!
+           currentTaps = 0
+           updateTapsLbl()
+            
+            
+        }
+        
+        
+        
+    }
+    func isGameOver() -> Bool{
+        if currentTaps >= maxTaps{
+            return true}
+        else{
+            return false}
+    }
+    
+    func restartGame(){
+        maxTaps = 0
+        howManyTapsTxt.text = ""
+        
+        logoimg.hidden = false
+        playBtn.hidden = false
+        howManyTapsTxt.hidden = false
+        tapsLbl.hidden = true
+        tapsBtn.hidden = true
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func updateTapsLbl(){
+         tapsLbl.text = "\(currentTaps) Taps"
+  
+    }
+    //This hides the keyboard
+    @IBAction func finishedTyping(sender: AnyObject) {
+         howManyTapsTxt.resignFirstResponder()
+    }
     }
 
+  
 
-}
+    
+
+
 
